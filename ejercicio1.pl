d8742355c4709af0b5da04b9ed58e26e85d8e3c5
martin-barrero <martin.barrero@utp.edu.co>
2025-02-20 09:43:42 -0500

    hechos.pl

--- ejercicio1.pl
+++ ejercicio1.pl
@@ -1,1 +1,9 @@
-
+conexion(vancouver, edmonton, 16).
+conexion(vancouver, calgary, 13).
+conexion(edmonton, saskatoon, 12).
+conexion(saskatoon, winnipeg, 20).
+conexion(saskatoon, calgary, 9).
+conexion(calgary, edmonton, 4).
+conexion(calgary, regina, 14).
+conexion(regina, saskatoon, 7).
+conexion(regina, winnioeg, 4).
