0b669ef9561d0950d6c20ce64c69f99b8218993f
Juan Fernando Cano <173503745+JuanF-Cano@users.noreply.github.com>
2025-02-20 10:03:49 -0500

    Update ejercicio1.pl
    regla, existe_conexion

--- ejercicio1.pl
+++ ejercicio1.pl
@@ -7,3 +7,5 @@ conexion(calgary, edmonton, 4).
 conexion(calgary, regina, 14).
 conexion(regina, saskatoon, 7).
 conexion(regina, winnipeg, 4).
+
+existe_conexion(A, B):- conexion(A, B, _); conexion(B, A, _).
